e8d2e5045c1e4b8e7c6dd4d36756ab3eca38824d
alexm0u <alexm0u.13@gmail.com>
2023-10-10 14:45:19 +0200

    First attempt to routing, children and lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404pageComponent } from './shared/pages/error404page/error404page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'panel',
+    loadChildren: () =>
+      import('./panel/panel.module').then((m) => m.PanelModule),
+  },
+  {
+    path: '404',
+    component: Error404pageComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'panel',
+    pathMatch: 'full'
+  },
+  {path: '**', redirectTo:'404'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/panel/panel-routing.module.ts
+++ src/app/panel/panel-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { CompaniesPageComponent } from './pages/companies-page/companies-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LayoutPageComponent, children: [
+    {
+      path: 'new-company', component: CompaniesPageComponent
+    }
+  ] }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
