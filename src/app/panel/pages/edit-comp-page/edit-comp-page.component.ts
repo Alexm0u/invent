9976c9ddcefa6140a6f8aec603d19f6c07dee1f5
alexm0u <alexm0u.13@gmail.com>
2023-10-14 21:55:27 +0200

    Add company works!!!!!!!!

--- src/app/panel/pages/edit-comp-page/edit-comp-page.component.ts
+++ src/app/panel/pages/edit-comp-page/edit-comp-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
+import { CompaniesService } from '../../services/companies.service';
+import { Company } from '../../interfaces/companies.interface';
 
 @Component({
   selector: 'app-edit-comp-page',
@@ -7,13 +9,35 @@ import { FormControl } from '@angular/forms';
   styles: [],
 })
 export class EditCompPageComponent {
-  public compForm = new FormControl({
-
-    id:           new FormControl(''),
-    name:         new FormControl(''),
-    direction:    new FormControl(''),
-    phone:        new FormControl(''),
-    postalCodes:  new FormControl(0),
-
+  public compForm = new FormGroup({
+    id: new FormControl<number>(0),
+    name: new FormControl<string>(''),
+    direction: new FormControl<string>(''),
+    phone: new FormControl<string>(''),
+    postalCodes: new FormControl<number[]>([]),
   });
+
+  constructor(private compService: CompaniesService) {}
+
+  get currentComp(): Company {
+    const comp = this.compForm.value as Company;
+    return comp;
+  }
+
+  onSubmit(): void {
+    if (this.compForm.invalid) return;
+
+    if (this.currentComp.id) {
+      this.compService.updateCompany(this.currentComp)
+        .subscribe(comp => {
+
+        });
+        return;
+    }
+    this.compService.addCompany(this.currentComp)
+      .subscribe(comp => {
+        // navegar a companies
+      })
+    // this.compService.updateCompany()
+  }
 }
