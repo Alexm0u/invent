9a7087ad23d1b6de3b89f67b9a66b9598d209c85
alexm0u <alexm0u.13@gmail.com>
2023-10-16 07:57:00 +0200

    Creating auth service

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environtments } from '.env/environtments';
+import { User } from '../interfaces/user.interface';
+import { tap, Observable } from 'rxjs'
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+
+    private baseUrl = environtments.baseUrl;
+    private user?: User;
+    constructor(private http: HttpClient) { }
+    
+    get currentUser(): User|undefined {
+        if (!this.user) return undefined;
+        return structuredClone (this.user);
+    }
+
+    login(email: string, password: string):Observable<User> {
+        return this.http.get<User>(`${this.baseUrl}/users/1`)
+            .pipe(
+                tap( user => this.user = user),
+                tap( user => localStorage.setItem('token', user.id.toString()))
+            );
+    }
+}
