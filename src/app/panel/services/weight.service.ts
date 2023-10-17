645f8974a3c6e3c666b7c2da10af789a4a733ee7
alexm0u <alexm0u.13@gmail.com>
2023-10-17 13:08:42 +0200

    first attempt to create services to assign zip codes and prices

--- src/app/panel/services/weight.service.ts
+++ src/app/panel/services/weight.service.ts
@@ -6,4 +6,20 @@ import { Injectable } from '@angular/core';
 export class WeightService {
 
   constructor() { }
+
+  calculatePrice(weight: number): number {
+    if (weight > 0 && weight <= 0.1) {
+      return weight * 5;
+    } else if (weight > 0.1 && weight <= 0.3) {
+      return weight * 5 + 1;
+    } else if (weight > 0.3 && weight <= 5) {
+      return weight * 10;
+    } else if (weight > 5 && weight <= 10) {
+      return weight * 5 + weight + 75;
+    } else if (weight > 10) {
+      return (weight - 10) * 7.5 + 130 + weight;
+    } else {
+      return 0;
+    }
+  }
 }
--- src/app/panel/services/zip.service.ts
+++ src/app/panel/services/zip.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ZipService {
+  constructor() {}
 
-  constructor() { }
+  assignCompanyByZipCode(postalCode: string): string {
+    const postalCodeNumber = parseInt(postalCode);
+    switch (true) {
+      case postalCodeNumber >= 15000 && postalCodeNumber <= 19999:
+        return '1'; 
+      case postalCodeNumber >= 20000 && postalCodeNumber <= 25999:
+        return '2'; 
+      default:
+        return '3'; 
+    }
+  }
+
+  getCompanyNameFromId(companyId: string): string {
+    switch (companyId) {
+      case '1':
+        return 'Correos';
+      case '2':
+        return 'Seur';
+      case '3':
+        return 'Invent';
+      default:
+        return 'Desconocido';
+    }
+  }
 }
